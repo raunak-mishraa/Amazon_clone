cc0fc10ed40ccfa1ceebe3f38220793784f9e39c
Raunak Mishra <107386749+raunak-mishraa@users.noreply.github.com>
2023-10-01 17:36:49 +0530

    added js files

---
+++ data/cart.js
@@ -0,0 +1,46 @@
+export let cart = JSON.parse(localStorage.getItem('cart'));
+if(!cart){//if cart is empty
+	cart = [{
+		productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+		quantity: 2
+	}, {
+		productId:'15b6fc6f-327a-4ec4-896f-486349e85a3d',
+		quantity:1
+	}];
+}
+
+
+function saveToStorage(){
+	localStorage.setItem('cart',JSON.stringify(cart));
+}
+
+export function addToCart(productId){
+	let matchingItem;//undefined
+	
+		cart.forEach((cartItem) => {
+			if(cartItem.productId === productId){
+				matchingItem = cartItem;//item is an object 
+			}
+		});
+		if(matchingItem){//if there is any machingItem then the quantity will increase
+			matchingItem.quantity += 1;
+		}
+		else{
+			cart.push({
+				productId:productId,
+				quantity:1
+			});
+		}
+
+		saveToStorage();
+}
+export function removeFromCart(productId){
+	const newCart = [];
+	cart.forEach((cartItem)=>{
+		if(cartItem.productId !== productId){
+			newCart.push(cartItem);
+		}
+	});
+	cart = newCart;
+	saveToStorage();
+}
